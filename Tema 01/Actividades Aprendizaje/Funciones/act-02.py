bdcdd16aad8cc185be7d5d7b3c02cef32bae3e44
Ángel Pereira <ampr2003@gmail.com>
2025-10-06 17:43:03 +0200

    Ejercicios Funciones - fix:act-02.py

--- Tema 01/Actividades Aprendizaje/Funciones/act-02.py
+++ Tema 01/Actividades Aprendizaje/Funciones/act-02.py
@@ -1,10 +1,17 @@
 # act-02.py
 # Archivo de ejercicios de funciones
 def EsMultiplo(numero1, numero2):
-  """Función que devuelve True si numero1 es múltiplo de numero2"""
+  """Función que devuelve True si numero1 es múltiplo de numero2 o viceversa"""
   if numero1 == 0 or numero2 == 0:
       return False
   return numero1 % numero2 == 0 or numero2 % numero1 == 0
 
-print(EsMultiplo(4, 2))
-print(EsMultiplo(2, 4))
+while True:
+    try:
+        num1 = int(input("Introduce el primer número: "))
+        num2 = int(input("Introduce el segundo número: "))
+
+        print(EsMultiplo(num1, num2))
+        break
+    except ValueError:
+        print("Por favor, introduce números enteros válidos.")
